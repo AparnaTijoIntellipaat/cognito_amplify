af8c3491e81d24a4e294f39d0931d7af5d271aa9
AparnaTijoIntellipaat <aparna.tijo@intellipaat.com>
2024-07-22 14:14:10 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
@@ -11,14 +12,17 @@ function App() {
     <div className="App">
       <div className="auth-wrapper">
         <Authenticator>
-          {({ signOut, user }) => (
+          {({ signOut }) => (
             <div className="auth-container">
               <header className="App-header">
-                <h1>Welcome, {user.username}</h1>
+                <h1>Welcome to My App</h1>
               </header>
               <main>
                 <p>You are logged in!</p>
-                <button onClick={signOut} className="sign-out-button">
+                <button
+                  onClick={signOut}
+                  className="sign-out-button"
+                >
                   Log Out
                 </button>
               </main>
